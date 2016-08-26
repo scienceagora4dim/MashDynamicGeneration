ff6a79a9b4ac6d78af04ebbf3cce8a6c01bb26de
muripoLife <life.is.too.high.difficulty@gmail.com>
2016-08-26 19:31:57 +0900

    Mesh動的生成サンプル

---
+++ Assets/Scripts/CreatePolygonMesh.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatePolygonMesh : MonoBehaviour
+{
+	private Mesh mesh;
+
+	// Use this for initialization
+	void Start()
+	{
+		mesh = new Mesh();
+		Vector3[] newVertices = new Vector3[4];
+		Vector2[] newUV = new Vector2[4];
+		int[] newTriangles = new int[2 * 3];
+
+		// 頂点座標の指定.
+		newVertices[0] = new Vector3(-2.0f, 0.0f, 2.0f);
+		newVertices[1] = new Vector3(-2.0f, 0.0f, -2.0f);
+		newVertices[2] = new Vector3(2.0f, 0.0f, -2.0f);
+		newVertices[3] = new Vector3(2.0f, 0.0f, 2.0f);
+
+		// UVの指定 (頂点数と同じ数を指定すること).
+		newUV[0] = new Vector2(0.0f, 0.0f);
+		newUV[1] = new Vector2(0.0f, 1.0f);
+		newUV[2] = new Vector2(1.0f, 1.0f);
+		newUV[3] = new Vector2(1.0f, 0.0f);
+
+		// 三角形ごとの頂点インデックスを指定.
+		newTriangles[0] = 2;
+		newTriangles[1] = 1;
+		newTriangles[2] = 0;
+		newTriangles[3] = 0;
+		newTriangles[4] = 3;
+		newTriangles[5] = 2;
+
+		mesh.vertices = newVertices;
+		mesh.uv = newUV;
+		mesh.triangles = newTriangles;
+
+		mesh.RecalculateNormals();
+		mesh.RecalculateBounds();
+
+		GetComponent<MeshFilter>().sharedMesh = mesh;
+		GetComponent<MeshFilter>().sharedMesh.name = "myMesh";
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//Mesh mesh = GetComponent<MeshFilter>().mesh;
+		//Vector3[] vertices = mesh.vertices;
+		//for (int i = 0; i < vertices.Length; i++)
+		//{
+		//	vertices[i].x += 0.01f;
+		//}
+		//mesh.vertices = vertices;
+	}
+}
---
+++ Assets/Shaders/Wave.shader
@@ -0,0 +1,29 @@
+﻿Shader "Custom/Wave" {
+	Properties{
+		_DiffuseColor("Diffuse Color",Color) = (0.0,0.0,1.0)
+	}
+
+	SubShader{
+		Tags{ "RenderType" = "Opaque" }
+		CGPROGRAM
+		#pragma surface surf Lambert vertex:vert
+		struct Input {
+			float4 color : COLOR;
+			float3 viewDir;
+		};
+		float4 _DiffuseColor;
+		void vert(inout appdata_full v) {
+			v.vertex.x += sin(v.vertex.x)*cos(v.vertex.y);
+			v.vertex.y += sin(v.vertex.x)*sin(v.vertex.y);
+			v.vertex.z += cos(v.vertex.x);
+
+			//v.vertex.x += cos(atan(v.vertex.z / v.vertex.x) + _Time.w);
+			//v.vertex.z += sin(atan(v.vertex.z / v.vertex.x) + _Time.w);
+		}
+		void surf(Input IN, inout SurfaceOutput o) {
+			o.Albedo = _DiffuseColor;
+		}
+		ENDCG
+	}
+	Fallback "Diffuse"
+}
